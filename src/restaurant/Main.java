2c3308187a15bc91410d14623cd4030461040e9b
MarinaLuchitska <lmarina3891@gmail.com>
2025-05-06 16:03:44 +0300

    Initial commit

---
+++ src/restaurant/Main.java
@@ -0,0 +1,16 @@
+package restaurant;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client(1, "Олена");
+        String[] menuItems = {"Піца", "Капучино", "Тірамісу"};
+        Order order = new Order(1001, client, menuItems);
+
+        Administrator admin = new Administrator("Ігор");
+
+        System.out.println(order);
+        admin.confirmOrder(order);
+        admin.acceptPayment(order);
+        System.out.println(order);
+    }
+}
